18e6c4a7f1761952f66d8250abfa12129baf3ada
fuzc4t4 <fuzc4t4@tutanota.com>
2023-12-13 02:13:19 -0600

    Added codium, fish, neofetch and firefox config.

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,88 @@
+# Set up the fish prompt
+function fish_prompt --description 'Write out the prompt'
+    # Save the last exit status
+    set -l laststatus $status
+    # Set normal text color
+    set -l normal (set_color normal)
+
+    # Get git information
+    set -l git_info
+    if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+        set git_branch (set_color -o blue)"$git_branch"
+        set -l git_status
+
+        # Check if there are uncommitted changes
+        if not command git diff-index --quiet HEAD --
+            if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
+                echo $count | read -l ahead behind
+
+                # Display arrows indicating the number of commits ahead and behind
+                if test "$ahead" -gt 0
+                    set git_status "$git_status"(set_color red)⬆
+                end
+                if test "$behind" -gt 0
+                    set git_status "$git_status"(set_color red)⬇
+                end
+            end
+
+            # Iterate through git status and display corresponding symbols
+            for i in (git status --porcelain | string sub -l 2 | sort | uniq)
+                switch $i
+                    case "."
+                        set git_status "$git_status"(set_color green)✚
+                    case " D"
+                        set git_status "$git_status"(set_color red)✖
+                    case "*M*"
+                        set git_status "$git_status"(set_color green)✱
+                    case "*R*"
+                        set git_status "$git_status"(set_color purple)➜
+                    case "*U*"
+                        set git_status "$git_status"(set_color brown)═
+                    case "??"
+                        set git_status "$git_status"(set_color red)≠
+                end
+            end
+        else
+            # No changes, display a colon
+            set git_status (set_color green):
+        end
+        set git_info "(git$git_status$git_branch"(set_color white)")"
+    end
+
+    # Disable PWD shortening by default.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
+    # Set background color to black and print the prompt
+    set_color -b black
+    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (prompt_pwd) (set_color white) $git_info (set_color white) '❱' (set_color white)
+
+    # Check last exit status and print corresponding symbols
+    if test $laststatus -eq 0
+        printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
+    else
+        printf "%s✘%s≻%s " (set_color -o red) (set_color white) (set_color normal)
+    end
+
+    # Starting a new line here
+    echo ""
+
+    # Color the prompt differently when we're root
+    set -l suffix '❯'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set cwd_color (set_color $fish_color_cwd_root)
+        end
+        set suffix '#'
+    end
+
+    # Color the prompt in red on error
+    if test $laststatus -ne 0
+        set status_color (set_color $fish_color_error)
+        set prompt_status $status_color "[" $last_status "]" $normal
+    end
+
+    # Print the prompt suffix and normal text
+    echo -s $status_color $suffix ' ' $normal
+end
+
--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # Dotfiles
 
-Dotfiles for i3 while keeping xfce4 installed.
-Inspired by parrot and archlabs default dotfiles.
-By default this script will setup your hostname as transient and randomize your mac address every reboot.
-
+Dotfiles for i3 while keeping xfce4 installed.  
+Inspired by parrot and archlabs default dotfiles.  
+By default this script will setup your hostname as transient and randomize your mac address every reboot.  
+Firefox ESR profile 'wildy' includes bookmarks from CSI Linux for OSINT investigations, custom cascade chrome css style, betterfox user.js integration and browser extensions to facilitate pentesting web apps.  
 Only Arch, openSUSE Tumbleweed and Debian Testing are being supported as for now.
 
 ## Usage
 Review and run install.sh
 
 ## To-Do:
-- [x] Script to auto install dependencies based on diffenrent distros
-- [ ] Support for more color schemes and a script to change them, currently only nord is available
-- [ ] Script to change the font configured, currently iosevka is configured
+- [x] Script to auto install dependencies based on diffenrent distros.
+- [ ] Support for more color schemes and a script to change them, currently only nord is available.
+- [ ] Script to change the font configured, currently iosevka is configured.
 - [ ] Add parameters to install.sh to make verbosity optional and integrate future color scheme and font selector scripts to it.
+- [ ] Include feature to install firefox esr and it's profiles(wildy).
+- [ ] GTK config files.
+- [ ] Xfce4 dotfiles.
 
 ## Some credits:
 * [github.com/polybar/polybar-scripts](https://github.com/polybar/polybar-scripts)
@@ -22,3 +25,6 @@ Review and run install.sh
 * [https://github.com/NvChad/NvChad](https://github.com/NvChad/NvChad)
 * [github.com/Qubes-Community/Contents/blob/master/docs/privacy/anonymizing-your-mac-address.md](https://github.com/Qubes-Community/Contents/blob/master/docs/privacy/anonymizing-your-mac-address.md)
 * [https://github.com/GuidoFe/weather-polybar-plugin](https://github.com/GuidoFe/weather-polybar-plugin)
+* [https://github.com/Chick2D/neofetch-themes](https://github.com/Chick2D/neofetch-themes)
+* [https://github.com/andreasgrafen/cascade](https://github.com/andreasgrafen/cascade)
+* [https://github.com/yokoffing/Betterfox](https://github.com/yokoffing/Betterfox)
