125957373fbd680727c0429e52bdc71b62d2294f
fuzc4t4 <fuzc4t4@tutanota.com>
2024-09-07 05:16:20 -0600

    Changed Nvchad for lazyvim. Deprecated firefox profile. Added Fedora support.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Dotfiles
 
 Dotfiles for i3 while keeping xfce4 installed.  
-Inspired by parrot and archlabs default dotfiles.  
-By default this script will setup your hostname as transient and randomize your mac address every reboot.  
-Firefox ESR profile 'wildy' includes bookmarks from CSI Linux for OSINT investigations, custom cascade chrome css style, betterfox user.js integration and browser extensions to facilitate pentesting web apps.  
-NvChad can be installed via install.sh but VsCodium config is given as well. VsCodium has to be independently installed as for now.  
-Only Arch, openSUSE Tumbleweed and Debian Testing are being supported as for now.
+By default this script will setup your hostname as transient and randomize your mac address.
+LazyVim can be installed via install.sh but VsCodium config is given as well.
+Works for Arch, openSUSE Tumbleweed, Debian Testing and Fedora.
 
 ## Usage
 Review and run install.sh
@@ -14,8 +12,6 @@ Review and run install.sh
 - [x] Script to auto install dependencies based on diffenrent distros.
 - [ ] Support for more color schemes and a script to change them, currently only nord is available.
 - [ ] Script to change the font configured, currently FantasqueSansM Nerd Font is configured.
-- [ ] Add parameters to install.sh to font and color scheme selector scripts to it.
-- [x] Include feature to install firefox esr profile(wildy).
 - [x] GTK config files.
 - [ ] Xfce4 dotfiles.
 
@@ -23,9 +19,7 @@ Review and run install.sh
 * [github.com/polybar/polybar-scripts](https://github.com/polybar/polybar-scripts)
 * [https://github.com/Murzchnvok/rofi-collection](https://github.com/Murzchnvok/rofi-collection)
 * [github.com/jluttine/rofi-power-menu](https://github.com/jluttine/rofi-power-menu)
-* [https://github.com/NvChad/NvChad](https://github.com/NvChad/NvChad)
+* [https://github.com/LazyVim/LazyVim](https://github.com/LazyVim/LazyVim)
 * [github.com/Qubes-Community/Contents/blob/master/docs/privacy/anonymizing-your-mac-address.md](https://github.com/Qubes-Community/Contents/blob/master/docs/privacy/anonymizing-your-mac-address.md)
 * [https://github.com/GuidoFe/weather-polybar-plugin](https://github.com/GuidoFe/weather-polybar-plugin)
 * [https://github.com/Chick2D/neofetch-themes](https://github.com/Chick2D/neofetch-themes)
-* [https://github.com/andreasgrafen/cascade](https://github.com/andreasgrafen/cascade)
-* [https://github.com/yokoffing/Betterfox](https://github.com/yokoffing/Betterfox)
--- install.sh
+++ install.sh
@@ -2,91 +2,93 @@
 
 # Function to ask user for Y/n input
 ask_user() {
-    local prompt="$1"
-    local response
-    read -p "$prompt (Y/n): " response
-    response=${response,,} # Convert to lowercase
-    if [[ "$response" == "y" ]]; then
-        echo "Y"
-    elif [[ "$response" == "n" ]]; then
-        echo "N"
-    else
-        echo "Invalid input. Please enter Y or n."
-        ask_user "$prompt"
-    fi
+  local prompt="$1"
+  local response
+  read -p "$prompt (Y/n): " response
+  response=${response,,} # Convert to lowercase
+  if [[ "$response" == "y" ]]; then
+    echo "Y"
+  elif [[ "$response" == "n" ]]; then
+    echo "N"
+  else
+    echo "Invalid input. Please enter Y or n."
+    ask_user "$prompt"
+  fi
 }
 
 # Function to install dependencies
 install_dependencies() {
-    if type 'pacman' > /dev/null 2>&1; then
-        sudo pacman -S fish kitty ranger bat pasystray pavucontrol playerctl picom xscreensaver rofi jgmenu lxmenu-data polybar i3-wm arandr lxrandr lxappearance zathura zathura-pdf-mupdf feh nitrogen neofetch neovim htop jq galculator arc-gtk-theme papirus-icon-theme nerd-fonts ripgrep
-    elif type 'zypper' > /dev/null 2>&1; then
-        sudo zypper in fish kitty ranger bat pasystray pavucontrol playerctl picom xscreensaver xscreensaver-data xscreensaver-data-extra rofi jgmenu lxmenu-data polybar i3 arandr lxrandr lxappearance zathura feh nitrogen neofetch neovim htop jq metatheme-arc-common papirus-icon-theme ripgrep
-    elif type 'apt' > /dev/null 2>&1; then
-        sudo apt install fish kitty ranger bat pasystray pavucontrol playerctl picom xscreensaver xscreensaver-data-extra xscreensaver-gl-extra rofi jgmenu lxmenu-data polybar i3-wm suckless-tools arandr lxrandr lxappearance zathura feh nitrogen neofetch htop jq galculator arc-theme papirus-icon-theme ripgrep
-        sudo dpkg -i system/nvim-linux64.deb
-    fi
+  if type 'dnf' >/dev/null 2>&1; then
+    sudo dnf in arandr arc-theme bat fd-find feh fish galculator htop i3 jgmenu jq kitty lxappearance lxmenu-data lxrandr neofetch neovim nitrogen papirus-icon-theme pasystray pavucontrol picom playerctl polybar ranger ripgrep rofi xscreensaver zathura
+  elif type 'apt' >/dev/null 2>&1; then
+    sudo apt install arandr arc-theme bat fd-find feh fish galculator htop i3-wm jgmenu jq kitty lxappearance lxmenu-data lxrandr neofetch neovim nitrogen papirus-icon-theme pasystray pavucontrol picom playerctl polybar ranger ripgrep rofi suckless-tools xscreensaver xscreensaver-data-extra xscreensaver-gl-extra zathura
+  elif type 'pacman' >/dev/null 2>&1; then
+    sudo pacman -S arandr arc-gtk-theme bat fd feh fish galculator htop i3-wm jgmenu jq kitty lxappearance lxmenu-data lxrandr neofetch neovim nerd-fonts nitrogen papirus-icon-theme pasystray pavucontrol picom playerctl polybar ranger ripgrep rofi xscreensaver zathura zathura-pdf-mupdf
+  elif type 'zypper' >/dev/null 2>&1; then
+    sudo zypper in arandr bat fd feh fish galculator htop i3 jgmenu jq kitty lxappearance lxmenu-data lxrandr metatheme-arc-common neofetch neovim nitrogen papirus-icon-theme pasystray pavucontrol picom playerctl polybar ranger ripgrep rofi xscreensaver xscreensaver-data xscreensaver-data-extra zathura
+  fi
 }
 
 # Function to install nerd fonts
 install_nerd_fonts() {
-    echo 'This will clone github.com/ryanoasis/nerd-fonts to .ignore/ in this directory and can take a while'
-    git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git .ignore/nerd-fonts/
-    echo 'Installing nerd-fonts'
-    bash .ignore/nerd-fonts/install.sh
-    echo 'Font configured by default is FantasqueSansM Nerd Font'
+  echo 'This will clone github.com/ryanoasis/nerd-fonts to .ignore/ in this directory and can take a while'
+  git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git .ignore/nerd-fonts/
+  echo 'Installing nerd-fonts'
+  bash .ignore/nerd-fonts/install.sh
+  echo 'Font configured by default is FantasqueSansM Nerd Font'
 }
 
 # Function to install dotfiles
 install_dotfiles() {
-    cp -Trv HOME/ ~/
+  cp -Trv HOME/ ~/
 }
 
-# Function to install nvchad
-install_nvchad() {
-    rm -rfv ~/.config/nvim
-    rm -rfv ~/.local/share/nvim
-    git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+# Function to install lazyvim
+install_lazyvim() {
+  rm -rfv ~/.config/nvim
+  rm -rfv ~/.local/share/nvim
+  rm -rfv ~/.cache/nvim
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  echo 'return {
+  { "shaunsingh/nord.nvim" },
+  { "rmehri01/onenord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+  }' >~/.config/nvim/lua/plugins/colorschemes.lua
 }
 
 # Function to install network configuration
 install_network_config() {
-    echo 'Setting up network configuration:'
-    sudo cp -v system/50-macrandomize.conf /etc/NetworkManager/conf.d/
-    loopbackName=$(cat /etc/hostname)
-    if [ ${#loopbackName} -gt 0 ]; then
-        sudo cp -v /dev/null /etc/hostname
-        sudo hostnamectl set-hostname --transient $loopbackName
-    fi
-    sudo systemctl restart NetworkManager
-}
-
-# Function to install Firefox profile
-install_firefox_profile() {
-    pkill firefox*
-    rm -rfv ~/.mozilla
-    tar xvf system/wildy.tar.bz2 -C ~/
+  echo 'Setting up network configuration:'
+  sudo cp -v system/50-macrandomize.conf /etc/NetworkManager/conf.d/
+  loopbackName=$(cat /etc/hostname)
+  if [ ${#loopbackName} -gt 0 ]; then
+    sudo cp -v /dev/null /etc/hostname
+    sudo hostnamectl set-hostname --transient $loopbackName
+  fi
+  sudo systemctl restart NetworkManager
 }
 
 # Main function
 main() {
-    echo "Choose tasks to perform:"
-    install_dependencies_var=$(ask_user "Install dependencies")
-    install_nerd_fonts_var=$(ask_user "Install nerd fonts")
-    install_dotfiles_var=$(ask_user "Install dotfiles")
-    install_nvchad_var=$(ask_user "Install nvchad")
-    install_network_config_var=$(ask_user "Install network configuration")
-    echo "Wildy Firefox profile is only compatible with Firefox ESR."
-    install_firefox_profile_var=$(ask_user "Install Firefox profile")
+  echo "Choose tasks to perform:"
+  install_dependencies_var=$(ask_user "Install dependencies")
+  install_nerd_fonts_var=$(ask_user "Install nerd fonts")
+  install_dotfiles_var=$(ask_user "Install dotfiles")
+  install_lazyvim_var=$(ask_user "Install lazyvim")
+  install_network_config_var=$(ask_user "Install network configuration")
 
-    [[ "$install_dependencies_var" == "Y" ]] && install_dependencies
-    [[ "$install_nerd_fonts_var" == "Y" ]] && install_nerd_fonts
-    [[ "$install_dotfiles_var" == "Y" ]] && install_dotfiles
-    [[ "$install_nvchad_var" == "Y" ]] && install_nvchad
-    [[ "$install_network_config_var" == "Y" ]] && install_network_config
-    [[ "$install_firefox_profile_var" == "Y" ]] && install_firefox_profile
+  [[ "$install_dependencies_var" == "Y" ]] && install_dependencies
+  [[ "$install_nerd_fonts_var" == "Y" ]] && install_nerd_fonts
+  [[ "$install_dotfiles_var" == "Y" ]] && install_dotfiles
+  [[ "$install_lazyvim_var" == "Y" ]] && install_lazyvim
+  [[ "$install_network_config_var" == "Y" ]] && install_network_config
 }
 
 # Run the main function
 main
-
